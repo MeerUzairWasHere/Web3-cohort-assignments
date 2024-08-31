721463c83684b253dee8eac6c19a0bea81f6d1d5
MeerUzairWasHere <meer.uxair007@gmail.com>
2024-09-01 00:37:17 +0530

    wip

--- week-5/SolDrop/src/main.jsx
+++ week-5/SolDrop/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Buffer } from "buffer/";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
