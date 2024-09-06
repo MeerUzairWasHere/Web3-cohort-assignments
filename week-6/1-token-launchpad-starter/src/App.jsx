c98424b4a17bfde72e4e4f39477debbc7dcd4c59
MeerUzairWasHere <meer.uxair007@gmail.com>
2024-09-06 21:18:05 +0530

    added create mint func

--- week-6/1-token-launchpad-starter/src/App.jsx
+++ week-6/1-token-launchpad-starter/src/App.jsx
@@ -1,10 +1,38 @@
-import './App.css'
-import { TokenLaunchpad } from './components/TokenLaunchpad'
+import "./App.css";
+import { TokenLaunchpad } from "./components/TokenLaunchpad";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 function App() {
   return (
-    <TokenLaunchpad></TokenLaunchpad>
-  )
+    <div>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: 20,
+              }}
+            >
+              <WalletMultiButton />
+              <WalletDisconnectButton />
+            </div>
+            <TokenLaunchpad></TokenLaunchpad>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
