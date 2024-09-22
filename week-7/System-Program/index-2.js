c5f3c3a8477156ecef17a5957b597b9166b4b46e
MeerUzairWasHere <meer.uxair007@gmail.com>
2024-09-22 19:35:57 +0530

    added title

--- week-7/System-Program/index-2.js
+++ week-7/System-Program/index-2.js
@@ -1,3 +1,4 @@
+// Transfer lamports from your account to another account
 const { Keypair, Connection, SystemProgram, Transaction } = require('@solana/web3.js');
 
 const payer = Keypair.fromSecretKey(Uint8Array.from([42,243,78,195,142,194,45,18,24,105,99,183,119,184,6,73,56,12,152,1,43,218,246,213,222,65,60,11,50,39,63,244,239,49,177,84,144,204,227,36,79,124,171,203,83,51,130,205,112,18,155,165,224,182,194,40,44,122,168,192,69,255,125,215]));
