6a8ead25c45cae54ccd105f8da0ba6cccf5c1244
MeerUzairWasHere <meer.uxair007@gmail.com>
2024-10-12 22:36:52 +0530

    init

---
+++ week-9/token-swapper/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Button>Token Swapper</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ week-9/token-swapper/vite.config.ts
@@ -0,0 +1,13 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+export default defineConfig({
+  plugins: [react(), nodePolyfills()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
